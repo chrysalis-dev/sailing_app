16236ce60b307a06a96a191da57bf816813350a7
Albert Viilik <albert.viilik@gmail.com>
2019-07-08 21:21:54 +0100

    Reformatted the Route Generator

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -7,8 +7,14 @@ import 'package:sailing_app/widgets/albert_temp_folder/competition_expanded.dart
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Arguments that are passed to the route on generation
+
     final args = settings.arguments;
+
+    // Switch statement that redirects to routes depending on the name passed
     switch (settings.name) {
+
+      // Navigate to the main page
       case MyHomePage.routeName:
         return MaterialPageRoute(
           builder: (context) => MyHomePage(
@@ -16,22 +22,30 @@ class RouteGenerator {
               ),
         );
 
+      // Navigate to a screen of an existing competition
       case CompIt.routeName:
         if (args is Competition) {
           return MaterialPageRoute(
-              builder: (context) => CompetitionScreen(
-                    competition: args,
-                  ));
+            builder: (context) => CompetitionScreen(
+                  competition: args,
+                ),
+          );
         }
-        return _errorRoute();
+        return _errorRoute(); // Return error if no competition passed as an argument
 
+      // Navigate to the the new competition creation dialog screen
       case CompDialog.routeName:
-        return MaterialPageRoute(builder: (context) => CompDialog());
+        return MaterialPageRoute(
+          builder: (context) => CompDialog(),
+        );
+
+      // By default, it redirects to an error screen
       default:
         return _errorRoute();
     }
   }
 
+  // Error screen
   static Route<dynamic> _errorRoute() {
     return MaterialPageRoute(builder: (BuildContext context) {
       return Scaffold(
